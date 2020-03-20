0780cf080f11b689f9ded23fa252bc9b7b3aa0d8
rohansolse <rohansolse@gmail.com>
2020-03-20 20:31:27 +0530

    add general init.sh

--- ubuntuInit.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-
-git pull origin master
-git add .
-echo "Enter your commit: "
-read commit
-git commit -m "$commit"
-git push origin master
--- windowsInit.sh
+++
@@ -1,6 +0,0 @@
-git pull origin master
-git add .
-echo "Enter your commit: "
-read $commit
-git commit -m "$commit"
-git push origin master
